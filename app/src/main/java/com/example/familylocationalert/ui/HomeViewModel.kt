39f9dac8a2a5c666dba63d3f8dfb0a9b7c800df8
André Oli <n0bledeath.117@gmail.com>
2026-08-12 11:52:18 +0100

    updated LocationService and homeview

--- app/src/main/java/com/example/familylocationalert/ui/HomeViewModel.kt
+++ app/src/main/java/com/example/familylocationalert/ui/HomeViewModel.kt
@@ -1,20 +1,43 @@
 package com.example.familylocationalert.ui
-
+import android.app.Application
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.AndroidViewModel
+import com.example.familylocationalert.service.LocationService
 
-class HomeViewModel : ViewModel() {
+class HomeViewModel(
+    application: Application
+) : AndroidViewModel(application) {
 
     var monitoring by mutableStateOf(false)
         private set
 
+    var latitude by mutableStateOf<Double?>(null)
+        private set
+
+    var longitude by mutableStateOf<Double?>(null)
+        private set
+
+    private val locationService = LocationService(getApplication())
+
     fun startMonitoring() {
+
         monitoring = true
+
+        locationService.startLocationUpdates { location ->
+
+            latitude = location.latitude
+            longitude = location.longitude
+        }
     }
 
     fun stopMonitoring() {
+
         monitoring = false
+
+        locationService.stopLocationUpdates()
     }
+
+
 }
